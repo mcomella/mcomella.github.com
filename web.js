5c8681a4639d2e845ff53dfcfd7fe3eb4ef603fb
Michael Comella <michael.l.comella@gmail.com>
2012-09-22 23:23:11 -0400

    Added CS course material to static file server.

--- web.js
+++ web.js
@@ -2,7 +2,7 @@ var static = require('node-static');
 var http = require('http');
 
 var file = new(static.Server)();
-var port = process.env.PORT || 8080;
+var port = process.env.PORT || 3000;
 
 http.createServer(function (req, res) {
   file.serve(req, res);

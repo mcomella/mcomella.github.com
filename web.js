614e50a094bd7a81c531632af4dd123515313e71
Michael Comella <michael.l.comella@gmail.com>
2012-09-22 22:52:50 -0400

    (Hopefully) fixed port problems.

--- web.js
+++ web.js
@@ -2,7 +2,8 @@ var static = require('node-static');
 var http = require('http');
 
 var file = new(static.Server)();
+var port = process.env.PORT || 8080;
 
 http.createServer(function (req, res) {
   file.serve(req, res);
-}).listen(8080);
+}).listen(port);

26dc0c9b1382ba46c7c3440161550469eae73cbf
Michael Comella <michael.l.comella@gmail.com>
2013-03-20 23:37:09 -0400

    Remove files necessary for Heroku hosting.

--- web.js
+++
@@ -1,9 +0,0 @@
-var static = require('node-static');
-var http = require('http');
-
-var file = new(static.Server)();
-var port = process.env.PORT || 3000;
-
-http.createServer(function (req, res) {
-  file.serve(req, res);
-}).listen(port);
